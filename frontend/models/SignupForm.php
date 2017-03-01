a2aff2865c463b6c6d4bc3f472f949dae8fbdb89
TrickTrick <alexeymarkov.x7@gmail.com>
2017-03-01 21:49:55 +0200

    finish validation

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -30,7 +30,11 @@ class SignupForm extends Model
             ['email', 'trim'],
             ['email', 'required'],
             ['email', 'email'],
-            ['email', 'match', 'pattern' => '/^([a-z0-9]{3,6}+([_\.\-]{1}[a-z0-9]+)*){1}([@]){1}([a-z0-9]+([_\-]{1}[a-z0-9]+)*)+(([\.]{1}[a-z]{2,6}((?!ru|ua).)){0,3}){1}$/'],
+            ['email', 'match', 'pattern' => '/^([a-z0-9]{3,}+([_\.\-]{1}[a-z0-9]+)*){1}([@]){1}([a-z0-9]+([_\-]{1}[a-z0-9]+)*)+(([\.]{1}[a-z]{2,6}){0,3}){1}$/'],
+            ['email', 'isAdequate', 'when' => function($model) {
+                $mailParts = explode('@', $model->email);
+                return preg_match('(\.ru|\.ua)', $mailParts[1]);
+            }],
             ['email', 'string', 'max' => 255],
             ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
 
@@ -41,29 +45,30 @@ class SignupForm extends Model
 
             ['birthday', 'required'],
             ['birthday', 'validDate'],
-            ['birthday', 'checkAge', 'when' => function($model) {
-                return in_array($model->country, [203, 'Ukraine', 173, 'Russian Federation']);
-            }],
-
         ];
     }
-
-    public function checkAge($attribute)
+    /**
+     * email depend of birthday validation
+     */
+    public function isAdequate($attribute)
     {
-        $dateModel = new \DateTime($this->$attribute);
-        $age = ($dateModel->diff(new \DateTime()));
-        if ((int)$age->format('%Y') <= self::AVAILABLE_AGE_FOR_SNG) {
+        $birthday = new \DateTime($this->birthday);
+        $currentDateTime = new \DateTime();
+        if((int)$birthday->diff($currentDateTime)->format('%Y') < self::AVAILABLE_AGE_FOR_SNG){
             $this->addError($attribute, 'When mom stops you give money to the buns, come to us.');
         }
     }
 
+    /**
+     * birthday validation
+     */
     public function validDate($attribute)
     {
         $attrTimestamp = (new \DateTime($this->$attribute))->getTimestamp();
         $currentTimestamp = (new \DateTime())->getTimestamp();
 
         if($attrTimestamp > $currentTimestamp){
-            $this->addError($attribute, 'DOB Must be valid.');
+            $this->addError($attribute, 'Day Of Birthday Must be valid.');
         }
     }
     /**
